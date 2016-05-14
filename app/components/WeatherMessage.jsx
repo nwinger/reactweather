a9c678602e016323e046469b7cee1f005dbd47a6
nwinger <winger.nicklas@gmail.com>
2016-05-14 23:40:13 +0200

    Add axios library and query real weather data from openweathermap.org

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,7 +4,7 @@ var WeatherMessage = React.createClass({
     render: function () {
         var {temp, location} = this.props;
         return (
-            <h3>It's {temp} in {location}.</h3>
+            <h3>It's {temp} celsius in {location}.</h3>
         );
     }
 });
