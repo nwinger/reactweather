77fc2b12ce9820ca05fc65554b75fa30b8fa6b8a
nwinger <winger.nicklas@gmail.com>
2016-05-14 23:07:39 +0200

    Three way dataflow from form to containr to message

--- public/bundle.js
+++ public/bundle.js
@@ -25755,17 +25755,36 @@
 	var Weather = React.createClass({
 	    displayName: 'Weather',
 
+	    getInitialState: function getInitialState() {
+	        return {
+	            location: 'Kristiansand',
+	            temp: 25
+	        };
+	    },
+
+	    handleSearch: function handleSearch(location) {
+	        this.setState({
+	            location: location,
+	            temp: 23
+	        });
+	    },
+
 	    render: function render() {
+	        var _state = this.state;
+	        var temp = _state.temp;
+	        var location = _state.location;
+
+
 	        return React.createElement(
 	            'div',
 	            null,
 	            React.createElement(
 	                'h3',
 	                null,
-	                'Weather Component'
+	                'Get Weather'
 	            ),
-	            React.createElement(WeatherForm, null),
-	            React.createElement(WeatherMessage, null)
+	            React.createElement(WeatherForm, { onSearch: this.handleSearch }),
+	            React.createElement(WeatherMessage, { temp: temp, location: location })
 	        );
 	    }
 	});
@@ -25776,25 +25795,36 @@
 /* 232 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 
 	var React = __webpack_require__(1);
 
 	var WeatherForm = React.createClass({
-	    displayName: "WeatherForm",
+	    displayName: 'WeatherForm',
+
+	    onFormSubmit: function onFormSubmit(e) {
+	        e.preventDefault();
+
+	        var location = this.refs.location.value;
+
+	        if (location.length > 0) {
+	            this.refs.location.value = '';
+	            this.props.onSearch(location);
+	        }
+	    },
 
 	    render: function render() {
 	        return React.createElement(
-	            "div",
+	            'div',
 	            null,
 	            React.createElement(
-	                "form",
-	                null,
-	                React.createElement("input", { type: "text" }),
+	                'form',
+	                { onSubmit: this.onFormSubmit },
+	                React.createElement('input', { type: 'text', ref: 'location' }),
 	                React.createElement(
-	                    "button",
+	                    'button',
 	                    null,
-	                    "Get Weather"
+	                    'Get Weather'
 	                )
 	            )
 	        );
@@ -25815,10 +25845,18 @@
 	    displayName: 'WeatherMessage',
 
 	    render: function render() {
+	        var _props = this.props;
+	        var temp = _props.temp;
+	        var location = _props.location;
+
 	        return React.createElement(
 	            'h3',
 	            null,
-	            'It\'s 35 in Kristiansand.'
+	            'It\'s ',
+	            temp,
+	            ' in ',
+	            location,
+	            '.'
 	        );
 	    }
 	});
