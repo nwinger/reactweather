b1c84d5898b32c56dd1b40a9db6b71332aa80bd1
nwinger <winger.nicklas@gmail.com>
2016-05-15 12:35:10 +0200

    Refactor components to stateless functional components using ES6 syntax

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,16 +1,14 @@
 var React = require('react');
 var Nav = require('Nav');
 
-var Main = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <Nav/>
-                <h3>Main component</h3>
-                {this.props.children}
-            </div>
-        );
-    }
-});
+var Main = (props) => {
+    return (
+        <div>
+            <Nav/>
+            <h3>Main component</h3>
+            {props.children}
+        </div>
+    );
+};
 
 module.exports = Main;
