469e2f850c8b70390d5d3aeee2f300a190c634e6
Nicklas Winger <winger.nicklas@gmail.com>
2016-05-18 16:34:26 +0200

    Add styling to main.jsx and its rendered children

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,11 @@ var Main = (props) => {
     return (
         <div>
             <Nav/>
-            <h3>Main component</h3>
-            {props.children}
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                    {props.children}
+                </div>
+            </div>
         </div>
     );
 };
