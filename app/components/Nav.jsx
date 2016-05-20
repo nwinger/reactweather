c0b0e9ee0d86010769c29c6e7dd535795407bd0b
Nicklas Winger <nwi@gecko.no>
2016-05-20 21:00:31 +0200

    Start wiring up search from NavBar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,25 +2,32 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
-    onSearch: function (e) {
-        e.preventDefault();
-        alert('Not yet wired up');
-    },
+  onSearch: function (e) {
+    e.preventDefault();
 
-    render: function () {
+    var location = this.refs.search.value;
+    var encodedLocation = encodeURIComponent(location);
+
+    if (location.length > 0) {
+      this.refs.search.value = '';
+      window.location.hash = '#/?location=' + encodedLocation;
+    }
+  },
+
+  render: function () {
     return (
       <div className="top-bar">
         <div className="top-bar-left">
           <ul className="menu">
             <li className="menu-text">React Weather App</li>
             <li>
-              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+              <IndexLink to="/" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Get Weather</IndexLink>
             </li>
             <li>
-              <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
+              <Link to="/about" activeClassName="active"  activeStyle={{ fontWeight: 'bold' }}>About</Link>
             </li>
             <li>
-              <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+              <Link to="/examples" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Examples</Link>
             </li>
           </ul>
         </div>
@@ -28,7 +35,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weather by city"/>
+                <input type="search" placeholder="Search weather by city" ref="search"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
@@ -38,7 +45,7 @@ var Nav = React.createClass({
         </div>
       </div>
     );
-    }
+  }
 });
 
 module.exports = Nav;
